a34f2b64d9595cef63d1bed869b43f736fed0fc3
Shantanu Patel <shantu.patel@gmail.com>
2024-12-28 16:27:05 +0530

    PropertyController class file updated by separating requests for all users and admin users along with new endpoint for creating a new property.

--- villa-agency-be/src/main/java/com/villa_agency/property/PropertyController.java
+++ villa-agency-be/src/main/java/com/villa_agency/property/PropertyController.java
@@ -3,13 +3,15 @@ package com.villa_agency.property;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.villa_agency.response.GenericResponse;
+
 @RestController
-@RequestMapping("/properties")
+// @RequestMapping("/properties")
 public class PropertyController {
 
 	private PropertyService propertyService;
@@ -19,10 +21,18 @@ public class PropertyController {
 		this.propertyService = propertyService;
 	}
 
-	@GetMapping
-	public ResponseEntity<List<Property>> getAllProperties() {
+	@GetMapping(path = "/all/properties")
+	public GenericResponse<List<Property>> getAllProperties() {
 		List<Property> properties = propertyService.getAllProperties();
 
-		return new ResponseEntity<>(properties, HttpStatus.OK);
+		// return new ResponseEntity<>(properties, HttpStatus.OK);
+		return new GenericResponse<>(HttpStatus.OK.value(), "List of properties retrieved successfully", properties);
+	}
+
+	@PostMapping(path = "/admin/properties")
+	public GenericResponse<String> createNewProperty(@RequestBody Property property) {
+		propertyService.createProperty(property);
+
+		return new GenericResponse<>(HttpStatus.CREATED.value(), "New property created successfully", null);
 	}
 }
