c3f2a9a6c2d3d9e314e1f44b916502d8798673a6
Shantanu Patel <shantu.patel@gmail.com>
2025-01-14 10:45:12 +0530

    AddressController class file updated by adding methods for put and delete mapping.

--- villa-agency-be/src/main/java/com/villa_agency/address/AddressController.java
+++ villa-agency-be/src/main/java/com/villa_agency/address/AddressController.java
@@ -3,9 +3,11 @@ package com.villa_agency.address;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.villa_agency.response.GenericResponse;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+// @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/addresses")
 public class AddressController {
 
@@ -37,5 +39,20 @@ public class AddressController {
 		addressService.createAddress(address);
 
 		return new GenericResponse<>(HttpStatus.CREATED.value(), "New Address created successfully", null);
+	}
+
+	@PutMapping("/{addressId}")
+	public GenericResponse<String> editExistingAddress(@PathVariable Long addressId, @RequestBody Address address) {
+		addressService.updateAddressById(addressId, address);
+
+		return new GenericResponse<>(HttpStatus.ACCEPTED.value(), "Address updated successfully", null);
+	}
+
+	@DeleteMapping("/{addressId}")
+	public GenericResponse<String> deleteExistingAddress(@PathVariable Long addressId) {
+		addressService.deleteAddress(addressId);
+
+		return new GenericResponse<>(HttpStatus.ACCEPTED.value(),
+				"Address deleted successfully", null);
 	}
 }
