b21672a728fb23d9ce0b3b203a1707e10b8f4048
Shantanu Patel <shantu.patel@gmail.com>
2025-01-14 10:55:13 +0530

    PropertyRepository interface updated with custom nested query method for finding address (trial method)

--- villa-agency-be/src/main/java/com/villa_agency/property/PropertyRepository.java
+++ villa-agency-be/src/main/java/com/villa_agency/property/PropertyRepository.java
@@ -1,9 +1,12 @@
 package com.villa_agency.property;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
 
+	Optional<Property> findByAddress_Id(Long id);
 }
