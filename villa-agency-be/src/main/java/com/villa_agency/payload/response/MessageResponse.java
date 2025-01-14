0b87704b43ba0a58fdb62925db74d6aa370a9d12
Shantanu Patel <shantu.patel@gmail.com>
2025-01-14 10:56:31 +0530

    MessageResponse class created for sending a response in case of bad request.

---
+++ villa-agency-be/src/main/java/com/villa_agency/payload/response/MessageResponse.java
@@ -0,0 +1,18 @@
+package com.villa_agency.payload.response;
+
+public class MessageResponse {
+
+	private String message;
+
+	public MessageResponse(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
